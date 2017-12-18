1c3ac1ea1e8d4a3d4bb5fd78014252737aaf4f18
joelee <as955133@gmail.com>
2017-12-18 19:56:18 +0800

    新增註解

--- CRUD/Form1.Designer.cs
+++ CRUD/Form1.Designer.cs
@@ -330,6 +330,7 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- CRUD/Form1.cs
+++ CRUD/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,30 @@ namespace CRUD
         {
             InitializeComponent();
         }
+        NorthwindEntities dc = new NorthwindEntities();
 
+
+        /// <summary>
+        /// Log 產生監視檔
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dc.Database.Log = Message =>
+            {
+                using (StreamWriter sw = File.AppendText("EFlog.txt"))
+                {
+                    sw.WriteLine(Message);
+                }
+            };
+        }
+        
+
+        
         private void btnQuery_Click(object sender, EventArgs e)//查詢事件
         {
-            NorthwindEntities dc = new NorthwindEntities();
             dataGridView1.DataSource = dc.Employees.Select(emp => new
             {
                 編號 = emp.EmployeeID,
@@ -32,13 +53,18 @@ namespace CRUD
 
         }
 
+        /// <summary>
+        /// dc.SaveChanges();儲存至DB
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAddEmployee_Click(object sender, EventArgs e)
         {
             Employees emp = new Employees();
             emp.FirstName = txtFirstName.Text;
             emp.LastName = txtLastName.Text;
             emp.Title = txtTitle.Text;
-            NorthwindEntities dc = new NorthwindEntities();
+
             dc.Employees.Add(emp);
             dc.SaveChanges();
             MessageBox.Show("新增成功");
@@ -48,8 +74,8 @@ namespace CRUD
 
         private void btnUpate_Click(object sender, EventArgs e)//更新事件
         {
-            NorthwindEntities dc = new NorthwindEntities();
-            Employees empUpdate = dc.Employees.Find(int.Parse(txtUpdateID.Text));
+
+            Employees empUpdate = dc.Employees.Find(int.Parse(txtUpdateID.Text));//員工編號需轉int
             empUpdate.FirstName = txtUpdateFirstName.Text;
             empUpdate.LastName = txtUpdateLastName.Text;
             empUpdate.Title = txtUpdateTitle.Text;
@@ -61,8 +87,8 @@ namespace CRUD
 
         private void btnFind_Click(object sender, EventArgs e)//查詢事件
         {
-            NorthwindEntities dc = new NorthwindEntities();
-            Employees empFind = dc.Employees.Find(int.Parse(txtUpdateID.Text));
+
+            Employees empFind = dc.Employees.Find(int.Parse(txtUpdateID.Text));//員工編號需轉int
             txtUpdateFirstName.Text = empFind.FirstName;
             txtUpdateLastName.Text = empFind.LastName;
             txtUpdateTitle.Text = empFind.Title;
@@ -70,14 +96,24 @@ namespace CRUD
 
         }
 
+
+        /// <summary>
+        /// MessageBoxButtons.YesNo,MessageBoxIcon.Question
+        /// 確認要顯示的事件(YesNo)按鈕，以及(Question)符號
+        /// DialogResult.Yes
+        /// 表示要傳回yes
+        /// dc.Employees.Find(Convert.ToInt32(txtEmployeeID.Text));
+        /// (Find)查詢(Convert.ToInt32)轉型的txtEmployeeID.Text)值
+        /// dc.Employees.Remove刪除DB資料來源(empDeleted);
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDelete_Click(object sender, EventArgs e)//刪除事件
-        {
-            
+        {           
 
             if(MessageBox.Show("確定刪除嗎?", "刪除確認", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
                                 ==DialogResult.Yes)
             {
-                NorthwindEntities dc = new NorthwindEntities();
                 Employees empDeleted = dc.Employees.Find(Convert.ToInt32(txtEmployeeID.Text));
                 if (empDeleted == null)
                 {
@@ -92,5 +128,6 @@ namespace CRUD
                 }
             }
         }
+
     }
 }
