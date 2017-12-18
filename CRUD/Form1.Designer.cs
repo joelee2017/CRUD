9a3cff0c7bd67b4e3e8bd7873baa1f52e6ed176f
joelee2017 <as955133@gmail.com>
2017-12-18 10:07:51 +0800

    新增、修改、刪除 PUSH

--- CRUD/Form1.Designer.cs
+++ CRUD/Form1.Designer.cs
@@ -29,15 +29,38 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnQuery = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnUpate = new System.Windows.Forms.Button();
+            this.txtUpdateID = new System.Windows.Forms.TextBox();
+            this.txtUpdateFirstName = new System.Windows.Forms.TextBox();
+            this.txtUpdateLastName = new System.Windows.Forms.TextBox();
+            this.txtUpdateTitle = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.btnDelect = new System.Windows.Forms.Button();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -53,16 +76,6 @@
             this.tabControl1.Size = new System.Drawing.Size(744, 549);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(736, 523);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "新增";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.btnQuery);
@@ -74,23 +87,15 @@
             this.tabPage1.Text = "查詢";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // btnQuery
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(736, 523);
-            this.tabPage3.TabIndex = 3;
-            this.tabPage3.Text = "修改";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // tabPage4
-            // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(736, 523);
-            this.tabPage4.TabIndex = 4;
-            this.tabPage4.Text = "刪除";
-            this.tabPage4.UseVisualStyleBackColor = true;
+            this.btnQuery.Location = new System.Drawing.Point(20, 13);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "查詢";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
             // 
             // dataGridView1
             // 
@@ -101,15 +106,221 @@
             this.dataGridView1.Size = new System.Drawing.Size(719, 469);
             this.dataGridView1.TabIndex = 0;
             // 
-            // btnQuery
+            // tabPage2
             // 
-            this.btnQuery.Location = new System.Drawing.Point(20, 13);
-            this.btnQuery.Name = "btnQuery";
-            this.btnQuery.Size = new System.Drawing.Size(75, 23);
-            this.btnQuery.TabIndex = 1;
-            this.btnQuery.Text = "查詢";
-            this.btnQuery.UseVisualStyleBackColor = true;
-            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            this.tabPage2.Controls.Add(this.txtFirstName);
+            this.tabPage2.Controls.Add(this.txtLastName);
+            this.tabPage2.Controls.Add(this.txtTitle);
+            this.tabPage2.Controls.Add(this.btnAddEmployee);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(736, 523);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "新增";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(113, 81);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(113, 115);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(113, 148);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // btnAddEmployee
+            // 
+            this.btnAddEmployee.Location = new System.Drawing.Point(113, 194);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(75, 23);
+            this.btnAddEmployee.TabIndex = 4;
+            this.btnAddEmployee.Text = "新增";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "職務：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "姓：";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btnUpate);
+            this.tabPage3.Controls.Add(this.txtUpdateID);
+            this.tabPage3.Controls.Add(this.txtUpdateFirstName);
+            this.tabPage3.Controls.Add(this.txtUpdateLastName);
+            this.tabPage3.Controls.Add(this.txtUpdateTitle);
+            this.tabPage3.Controls.Add(this.btnFind);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(736, 523);
+            this.tabPage3.TabIndex = 3;
+            this.tabPage3.Text = "修改";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnUpate
+            // 
+            this.btnUpate.Location = new System.Drawing.Point(179, 195);
+            this.btnUpate.Name = "btnUpate";
+            this.btnUpate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpate.TabIndex = 18;
+            this.btnUpate.Text = "更新";
+            this.btnUpate.UseVisualStyleBackColor = true;
+            this.btnUpate.Click += new System.EventHandler(this.btnUpate_Click);
+            // 
+            // txtUpdateID
+            // 
+            this.txtUpdateID.Location = new System.Drawing.Point(111, 50);
+            this.txtUpdateID.Name = "txtUpdateID";
+            this.txtUpdateID.Size = new System.Drawing.Size(100, 22);
+            this.txtUpdateID.TabIndex = 17;
+            // 
+            // txtUpdateFirstName
+            // 
+            this.txtUpdateFirstName.Location = new System.Drawing.Point(111, 83);
+            this.txtUpdateFirstName.Name = "txtUpdateFirstName";
+            this.txtUpdateFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtUpdateFirstName.TabIndex = 16;
+            // 
+            // txtUpdateLastName
+            // 
+            this.txtUpdateLastName.Location = new System.Drawing.Point(111, 117);
+            this.txtUpdateLastName.Name = "txtUpdateLastName";
+            this.txtUpdateLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtUpdateLastName.TabIndex = 15;
+            // 
+            // txtUpdateTitle
+            // 
+            this.txtUpdateTitle.Location = new System.Drawing.Point(111, 150);
+            this.txtUpdateTitle.Name = "txtUpdateTitle";
+            this.txtUpdateTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtUpdateTitle.TabIndex = 14;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(62, 195);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 13;
+            this.btnFind.Text = "查詢";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "職務：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "名：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "姓：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(33, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "編號：";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.btnDelect);
+            this.tabPage4.Controls.Add(this.txtEmployeeID);
+            this.tabPage4.Controls.Add(this.label1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(736, 523);
+            this.tabPage4.TabIndex = 4;
+            this.tabPage4.Text = "刪除";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // btnDelect
+            // 
+            this.btnDelect.Location = new System.Drawing.Point(129, 118);
+            this.btnDelect.Name = "btnDelect";
+            this.btnDelect.Size = new System.Drawing.Size(75, 23);
+            this.btnDelect.TabIndex = 20;
+            this.btnDelect.Text = "刪除";
+            this.btnDelect.UseVisualStyleBackColor = true;
+            this.btnDelect.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(111, 60);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(100, 22);
+            this.txtEmployeeID.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "編號：";
             // 
             // Form1
             // 
@@ -122,6 +333,12 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -135,6 +352,26 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpate;
+        private System.Windows.Forms.TextBox txtUpdateID;
+        private System.Windows.Forms.TextBox txtUpdateFirstName;
+        private System.Windows.Forms.TextBox txtUpdateLastName;
+        private System.Windows.Forms.TextBox txtUpdateTitle;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnDelect;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CRUD/Form1.cs
+++ CRUD/Form1.cs
@@ -17,7 +17,7 @@ namespace CRUD
             InitializeComponent();
         }
 
-        private void btnQuery_Click(object sender, EventArgs e)
+        private void btnQuery_Click(object sender, EventArgs e)//查詢事件
         {
             NorthwindEntities dc = new NorthwindEntities();
             dataGridView1.DataSource = dc.Employees.Select(emp => new
@@ -25,10 +25,66 @@ namespace CRUD
                 編號 = emp.EmployeeID,
                 姓 = emp.FirstName,
                 名 = emp.LastName,
+                職務 = emp.Title,
 
             }).ToArray();
 
 
+        }
+
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            Employees emp = new Employees();
+            emp.FirstName = txtFirstName.Text;
+            emp.LastName = txtLastName.Text;
+            emp.Title = txtTitle.Text;
+            NorthwindEntities dc = new NorthwindEntities();
+            dc.Employees.Add(emp);
+            dc.SaveChanges();
+            MessageBox.Show("新增成功");
+            btnQuery_Click(sender, e);//查詢事件呼叫
+
+        }
+
+        private void btnUpate_Click(object sender, EventArgs e)//更新事件
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            Employees empUpdate = dc.Employees.Find(int.Parse(txtUpdateID.Text));
+            empUpdate.FirstName = txtUpdateFirstName.Text;
+            empUpdate.LastName = txtUpdateLastName.Text;
+            empUpdate.Title = txtUpdateTitle.Text;
+            dc.Entry(empUpdate).State = System.Data.Entity.EntityState.Modified;
+            dc.SaveChanges();
+            MessageBox.Show("更新成功");
+            btnQuery_Click(sender, e);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)//查詢事件
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            Employees empFind = dc.Employees.Find(int.Parse(txtUpdateID.Text));
+            txtUpdateFirstName.Text = empFind.FirstName;
+            txtUpdateLastName.Text = empFind.LastName;
+            txtUpdateTitle.Text = empFind.Title;
+           
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)//刪除事件
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            Employees empDeleted = dc.Employees.Find(Convert.ToInt32(txtEmployeeID.Text));
+            if(empDeleted==null)
+            {
+                MessageBox.Show($"無  {txtEmployeeID.Text}  員工記錄");
+            }
+            else
+            {
+                dc.Employees.Remove(empDeleted);
+                dc.SaveChanges();
+                MessageBox.Show("刪除成功");
+                btnQuery_Click(sender, e);
+            }
         }
     }
 }
